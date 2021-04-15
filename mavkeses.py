63024f9d88a7c07bb89baa8fd20a463750ec547c
rglszl <rglszl@gmail.com>
2021-04-15 09:40:28 +0200

    Update mavkeses.py

--- mavkeses.py
+++ mavkeses.py
@@ -1,53 +1,101 @@
-#!/usr/bin/env python3
+from flask import Flask, render_template
+import threading
+import time
+from datetime import datetime
 import json
 import requests
-import flask
-from flask import jsonify, Response
-from qovery_client.qovery import Qovery
+import mysql.connector
 
-# --- START INIT ---
+class backgroundTask:
+    def __init__(self):
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True
+        thread.start()
+        
+    def run(self):
+        while(True):
+            url="http://vonatinfo.mav-start.hu/map.aspx/getData"
+            headers={'Content-Type': 'application/json'}
+            data='{"a":"TRAINS","jo":{"pre":true,"history":false,"id":false}}'
 
-app = flask.Flask(__name__)
+            jsondata = json.loads(requests.post(url, headers=headers, data=data).text)
 
-# this file is not used while deployed on Qovery
-configuration_file_path = '../.qovery/local_configuration.json'
+            numberOfTrainsGysev=0
+            numberOfTrainsMav=0
+            numberOfTrains=0
+            delaySumGysev=0
+            delaySumMav=0
+            delaySum=0
+            maxDelayGysev=0
+            maxDelayMav=0
+            maxDelay=0
+            for i in jsondata["d"]["result"]["Trains"]["Train"]:
+                for j in i:
+                    try:
+                        if maxDelay<int(i["@Delay"]):
+                            maxDelay=int(i["@Delay"])
+                        delaySum=delaySum+int(i["@Delay"])
+                        numberOfTrains=numberOfTrains+1
+                        
+                        if i["@Menetvonal"] == "MAV":
+                            if maxDelayMav<int(i["@Delay"]):
+                                maxDelayMav=int(i["@Delay"])
+                            delaySumMav=delaySumMav+int(i["@Delay"])
+                            numberOfTrainsMav=numberOfTrainsMav+1
+                        if i["@Menetvonal"] == "GYSEV":
+                            if maxDelayGysev<int(i["@Delay"]):
+                                maxDelayGysev=int(i["@Delay"])
+                            delaySumGysev=delaySumGysev+int(i["@Delay"])
+                            numberOfTrainsGysev=numberOfTrainsGysev+1
+                        
+                    except:
+                        pass
+            mydb = mysql.connector.connect(
+                    host="kesesek-qw7ou1ufan0eb3lj-svc.qovery.io",
+                    user="superuser",
+                    password="8x8IPd65GUJgF5SU",
+                    database="mysql-kesesek",
+                    buffered=True
+                )
+            cursor = mydb.cursor()
+            insert_into_query = """
+            INSERT INTO {} (timestamp, keses, max) VALUES ( UNIX_TIMESTAMP(CONVERT_TZ(LOCALTIME(), '+00:00', @@session.time_zone)), '%s', '%s' )
+            """
+            cursor.execute(insert_into_query.format("minden"), (round(delaySum/numberOfTrains, 2), maxDelay))
+            cursor.execute(insert_into_query.format("mav"), (round(delaySumMav/numberOfTrainsMav, 2), maxDelayMav))
+            cursor.execute(insert_into_query.format("gysev"), (round(delaySumGysev/numberOfTrainsGysev, 2), maxDelayGysev))
+            mydb.commit()
 
-# get database configuration from Qovery
-qovery = Qovery(configuration_file_path=configuration_file_path)
+            
+            time.sleep(300)
+        
+         
 
-def getData():
-    url="http://vonatinfo.mav-start.hu/map.aspx/getData"
-    headers={'Content-Type': 'application/json'}
-    data='{"a":"TRAINS","jo":{"pre":true,"history":false,"id":false}}'
+app = Flask(__name__)
 
-    jsondata = json.loads(requests.post(url, headers=headers, data=data).text)
+@app.route('/keses')
+def keses():
+    mydb = mysql.connector.connect(
+        host="kesesek-qw7ou1ufan0eb3lj-svc.qovery.io",
+        user="superuser",
+        password="8x8IPd65GUJgF5SU",
+        database="mysql-kesesek",
+        buffered=True
+    )
+    cursor = mydb.cursor()
+    cursor.execute("SELECT KESES, MAX, TIMESTAMP FROM minden ORDER BY TIMESTAMP DESC LIMIT 1")
+    (avgAll, maxAll, ts) = cursor.fetchone()
+    cursor.execute("SELECT KESES, MAX FROM mav ORDER BY TIMESTAMP DESC LIMIT 1")
+    (avgMav, maxMav) = cursor.fetchone()
+    cursor.execute("SELECT KESES, MAX FROM gysev ORDER BY TIMESTAMP DESC LIMIT 1")
+    (avgGysev, maxGysev) = cursor.fetchone()
+    return render_template("template.html", ts=datetime.fromtimestamp(int(ts)), avgAll=avgAll, maxAll=maxAll,avgMav=avgMav, maxMav=maxMav,avgGysev=avgGysev,maxGysev=maxGysev)
+                   
+def main():
+    try:
+        begin = backgroundTask()
+    except:
+        abort(500)
+    app.run(host='0.0.0.0',threaded=True)
 
-    numberOfTrains=0
-    delaySum=0
-    maxDelay=0
-    for i in jsondata["d"]["result"]["Trains"]["Train"]:
-        for j in i:
-            try:
-                if maxDelay<int(i["@Delay"]):
-                    maxDelay=int(i["@Delay"])
-                delaySum=delaySum+int(i["@Delay"])
-                numberOfTrains=numberOfTrains+1
-            except:
-                pass
-    return maxDelay, delaySum/numberOfTrains
-
-
-@app.route('/atlagos')
-def atlagos():
-    maxDelay, avgDelay = getData()
-    return str(avgDelay)
-
-@app.route('/max')
-def max():
-    maxDelay, avgDelay = getData()
-    return str(maxDelay)
-
-
-if __name__ == '__main__':
-    print('Server is ready!')
-    app.run(host='0.0.0.0', port=5000)
+main()
